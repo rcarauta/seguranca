10c109e22ebd6d2b58a73ff4f803cda36b631939
Renato Carauta Ribeiro <rcarauta@unb.br>
2017-05-03 18:21:39 -0300

    Criação de um esquema de segurança com redirecionamento pelo arquivo de configuração url_security.json

--- app/_redirect/redirect.service.ts
+++ app/_redirect/redirect.service.ts
@@ -1,6 +1,7 @@
 import {Location} from '@angular/common';
 import { Injectable } from '@angular/core';
 import {AuthenticationService} from "../_services/authentication.service";
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class RedirectService {
@@ -9,43 +10,64 @@ export class RedirectService {
 
   public timeSession: number = 3600;
 
-
   public localDateTime: number;
 
   private error:string = '';
 
-  constructor(private authenticationService: AuthenticationService, private loc: Location){
+  constructor(private authenticationService: AuthenticationService, private loc: Location, private activatedRoute: ActivatedRoute){
     this.location = loc;
   }
 
   initVerificationRedirect() {
-    if(localStorage.getItem("dateAccessPage")){
+    if(sessionStorage.getItem("dateAccessPage") && this.authenticationService.currentUser.token != ""){
       let dateSecoundAccess = Date.now();
-      this.localDateTime = Number(localStorage.getItem("dateAccessPage"));
+      this.localDateTime = Number(sessionStorage.getItem("dateAccessPage"));
       let value = dateSecoundAccess - this.localDateTime;
-      if(value >= (this.timeSession * 1000)){
+      if(value >= (this.timeSession * 3600)){
         this.authenticationService.logout();
       }
 
     }else{
-      this.localDateTime = Date.now();
-      localStorage.setItem("dateAccessPage",this.localDateTime.toString());
+      if(this.authenticationService.currentUser.token) {
+        this.authenticationService.periodicIncrement(3600);
+        this.localDateTime = Date.now();
+        sessionStorage.setItem("dateAccessPage",this.localDateTime.toString());
+      }else {
+        this.authenticateClient();
+      }
+
     }
 
-    if(localStorage.getItem('currentUser')) {
-      let sessionTime = JSON.parse(localStorage.getItem('currentUser'));
-      this.authenticationService.periodicIncrement(this.timeSession);
-    }else {
-      this.authenticateClient();
-    }
+  }
+  
+  redirectWithCodeUrl(code:string) {
+    this.authenticationService.getUrlUser('/seguranca/url_security.json')
+      .subscribe(resultado =>{
+        var url = resultado.url;
+        this.authenticationService.redirectUserTokenAccess(url, resultado.client_id, resultado.client_secret,code,
+          resultado.grant_type, resultado.url_redirect)
+          .subscribe(resultado => {
+            console.log('Funcionou !!');
+          })
+      });
   }
 
   authenticateClient(){
-    this.authenticationService.getUrl('/seguranca/url_security.json')
-      .subscribe(resultado =>{
-        localStorage.setItem('authorization',JSON.stringify(resultado.authorization));
-        window.location.href = resultado.url;
-      });
+    if(this.authenticationService.currentUser.token == '') {
+      this.authenticationService.logout();
+      this.authenticationService.getUrl('/seguranca/url_security.json')
+        .subscribe (resultado => {
+          window.location.href = resultado.url;
+        });
+    } else {
+      this.authenticationService.getUrl('/seguranca/url_security.json')
+        .subscribe (resultado => {
+          if(resultado.store == 'variable'){
+            this.authenticationService.currentUser.authorization = resultado.authorization;
+            localStorage.removeItem('externalFile');
+          }
+        });
+    }
   }
 
 
